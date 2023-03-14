367ae4797211aaa911a451b2e2f71c278576b420
Dhruv Patel <80629263+DhruvPatel96@users.noreply.github.com>
2023-03-14 18:22:15 -0400

    Docter Delete

--- src/pages/clinic/dashboard/Overview.tsx
+++ src/pages/clinic/dashboard/Overview.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Container, Card, CardContent, Typography } from "@mui/material";
 
-const Overview = () => {
-	return <div>Overview</div>;
+interface CardProps {
+	title: string;
+	content: string;
+}
+
+const CardComponent: React.FC<CardProps> = ({ title, content }) => {
+	return (
+		<Card>
+			<CardContent>
+				<Typography variant="h5" component="h2">
+					{title}
+				</Typography>
+				<Typography sx={{ mb: 1.5 }} color="text.secondary">
+					{content}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+};
+
+const Overview: React.FC = () => {
+	return (
+		<Container>
+			<CardComponent
+				title="Card 1"
+				content="This is the content for Card 1"
+			/>
+			<CardComponent
+				title="Card 2"
+				content="This is the content for Card 2"
+			/>
+			<CardComponent
+				title="Card 3"
+				content="This is the content for Card 3"
+			/>
+		</Container>
+	);
 };
 
 export default Overview;
