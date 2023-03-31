d165b399555a7998b7faa1b76d818fa926264875
Harmanjot Singh <harmanjotsuri@gmail.com>
2023-03-31 02:12:07 -0400

    Modified the code for Client and Clinic Profile0

--- src/pages/client/Profile.tsx
+++ src/pages/client/Profile.tsx
@@ -10,38 +10,8 @@ import Container from "@mui/material/Container";
 import { Grid, Tab, Tabs, TextField } from "@mui/material";
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 function ClientProfile() {
   useEffect(() => {
     fetchClientData();
@@ -51,12 +21,14 @@ function ClientProfile() {
     const db = getFirestore();
     const ref = doc(db, "Client Record", "harmanjotsuri@gmail.com");
     const docSnap = await getDoc(ref);
-    let fullName = document.getElementById("fullname") as HTMLInputElement;
-    let email = document.getElementById("email") as HTMLInputElement;
-    let contact = document.getElementById("contact") as HTMLInputElement;
-    let city = document.getElementById("city") as HTMLInputElement;
-    let street = document.getElementById("street") as HTMLInputElement;
-    let province = document.getElementById("province") as HTMLInputElement;
+    let fullName = document.getElementById("clinicName") as HTMLInputElement;
+    let email = document.getElementById("clinicEmail") as HTMLInputElement;
+    let contact = document.getElementById("clinicContact") as HTMLInputElement;
+    let street = document.getElementById("clinicStreet") as HTMLInputElement;
+    let city = document.getElementById("clinicCity") as HTMLInputElement;
+    let province = document.getElementById(
+      "clinicProvince"
+    ) as HTMLInputElement;
     let address = document.getElementById("address") as HTMLInputElement;
     let password = document.getElementById("password") as HTMLInputElement;
     let confirmpass = document.getElementById(
@@ -68,8 +40,8 @@ function ClientProfile() {
       fullName.value = docSnap.data().Name;
       email.value = docSnap.data().email;
       contact.value = docSnap.data().phone;
-      city.value = docSnap.data().city;
       street.value = docSnap.data().street;
+      city.value = docSnap.data().city;
       province.value = docSnap.data().province;
       address.value =
         docSnap.data().street +
@@ -84,14 +56,16 @@ function ClientProfile() {
 
   async function updateDoc_Client() {
     const db = getFirestore();
-    let email = document.getElementById("email") as HTMLInputElement;
+    let email = document.getElementById("clinicEmail") as HTMLInputElement;
 
     const ref = doc(db, "Client Record", email.value);
-    let fullName = document.getElementById("fullname") as HTMLInputElement;
-    let contact = document.getElementById("contact") as HTMLInputElement;
-    let city = document.getElementById("city") as HTMLInputElement;
-    let street = document.getElementById("street") as HTMLInputElement;
-    let province = document.getElementById("province") as HTMLInputElement;
+    let fullName = document.getElementById("clinicName") as HTMLInputElement;
+    let contact = document.getElementById("clinicContact") as HTMLInputElement;
+    let street = document.getElementById("clinicStreet") as HTMLInputElement;
+    let city = document.getElementById("clinicCity") as HTMLInputElement;
+    let province = document.getElementById(
+      "clinicProvince"
+    ) as HTMLInputElement;
     let address = document.getElementById("address") as HTMLInputElement;
     let password = document.getElementById("password") as HTMLInputElement;
     let confirmpass = document.getElementById(
@@ -104,6 +78,7 @@ function ClientProfile() {
       street: street.value,
       city: city.value,
       province: province.value,
+
       confirmPass: confirmpass.value,
     })
       .then(() => {
@@ -114,164 +89,153 @@ function ClientProfile() {
       });
   }
 
-  const [value, setValue] = React.useState(0);
+  // const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  // const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  //   setValue(newValue);
+  // };
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/client/auth");
   };
   return (
-    <>
-      <React.Fragment>
-        <CssBaseline />
-        <Container fixed>
-          <Box
-            sx={{
-              display: "flex",
-              // borderRadius: 2,
-              marginTop: 5,
-              boxShadow: 12,
-              bgcolor: "#FFFFFF",
-              "& > :not(style)": {
-                width: "85vw",
-                height: "75vh",
-              },
-            }}
-          >
-            <Paper elevation={3}>
-              <TextField
-                id="standard-read-only-input"
-                defaultValue="Walk in Easy : Client Profile "
-                InputProps={{
-                  readOnly: true,
-                  style: {
-                    paddingLeft: "20px",
-                    fontWeight: "bold",
-                    fontSize: "25px",
-                    color: "#0089ED",
-                  },
-                }}
-                // margin="dense"
-                fullWidth
-                size="medium"
-                variant="standard"
-              />
-              <Box sx={{ width: "100%" }}>
-                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                  <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="basic tabs example"
-                  >
-                    <Tab label="About" {...a11yProps(0)} />
-                    <Tab label="Address" {...a11yProps(1)} />
-                  </Tabs>
-                </Box>
-                <TabPanel value={value} index={0}>
-                  <Grid
-                    container
-                    rowSpacing={1}
-                    columnSpacing={2}
-                    padding={4}
-                    marginTop={1}
-                  >
-                    <Grid item xs={2} sm={4}>
-                      <label>Full Name *</label>
-                      <TextField
-                        id="fullname"
-                        required
-                        autoFocus
-                        variant="standard"
-                        placeholder="Enter your Name"
-                        fullWidth
-                      />
-                    </Grid>
-                    <Grid item xs={2} sm={4}>
-                      <label>Email Id *</label>
-                      <TextField
-                        disabled
-                        id="email"
-                        type="email"
-                        variant="standard"
-                        fullWidth
-                        placeholder="Enter your Email"
-                      />
-                    </Grid>
-                    <Grid item xs={2} sm={4}>
-                      <label>Contact No. *</label>
-                      <TextField
-                        required
-                        id="contact"
-                        type="number"
-                        placeholder="Enter your Number"
-                        variant="standard"
-                        fullWidth
-                      />
-                    </Grid>
-                  </Grid>
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                  <Grid
-                    container
-                    rowSpacing={1}
-                    columnSpacing={2}
-                    padding={4}
-                    marginTop={1}
-                  >
-                    <Grid item xs={12} sm={4}>
-                      <label>Street*</label>
-                      <TextField
-                        required
-                        id="street"
-                        variant="standard"
-                        fullWidth
-                        placeholder="Enter your Street"
-                      />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                      <label>City *</label>
-                      <TextField
-                        required
-                        id="city"
-                        variant="standard"
-                        fullWidth
-                        placeholder="Enter your City"
-                      />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                      <label>Province *</label>
-                      <TextField
-                        required
-                        id="province"
-                        variant="standard"
-                        fullWidth
-                        placeholder="Enter your Province"
-                      />
-                    </Grid>
-                  </Grid>
-                </TabPanel>
-              </Box>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Box
+          sx={{
+            display: "flex",
+            // borderRadius: 2,
+            marginTop: 1,
+            boxShadow: 12,
+            bgcolor: "#FFFFFF",
+            "& > :not(style)": {
+              width: "85vw",
+              height: "75vh",
+            },
+          }}
+        >
+          <Paper elevation={3}>
+            <TextField
+              id="standard-read-only-input"
+              defaultValue="Walk in Easy : Clinic Profile"
+              InputProps={{
+                readOnly: true,
+                style: {
+                  paddingLeft: "20px",
+                  fontWeight: "bold",
+                  fontSize: "25px",
+                  color: "#0089ED",
+                },
+              }}
+              margin="dense"
+              fullWidth
+              size="medium"
+              variant="standard"
+            />
+            <Grid
+              container
+              rowSpacing={1}
+              columnSpacing={2}
+              padding={4}
+              //   marginTop={2}
+            >
+              <Grid item xs={12} sm={4}>
+                <label>Organization *</label>
+                <TextField
+                  id="clinicName"
+                  required
+                  autoFocus
+                  variant="standard"
+                  placeholder="Enter your Organization"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <label>Clinic Email Id *</label>
+                <TextField
+                  disabled
+                  id="clinicEmail"
+                  type="email"
+                  variant="standard"
+                  fullWidth
+                  placeholder="Enter your Email"
+                />
+              </Grid>
 
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ mr: 9, marginLeft: 55, border: 1, boxShadow: 3 }}
-              >
-                Cancel
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={updateDoc_Client}
-                sx={{ border: 1, boxShadow: 3 }}
-              >
-                Save Profile
-              </Button>
-              <br></br>
-            </Paper>
-          </Box>
-        </Container>
-      </React.Fragment>
-    </>
+              <Grid item xs={12} sm={4}>
+                <label>Contact No. *</label>
+                <TextField
+                  required
+                  id="clinicContact"
+                  type="number"
+                  placeholder="Enter your Number"
+                  variant="standard"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <p>Address</p>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <label>Street *</label>
+                <TextField
+                  required
+                  id="clinicStreet"
+                  variant="standard"
+                  fullWidth
+                  placeholder="Enter your Street"
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <label>City *</label>
+                <TextField
+                  required
+                  id="clinicCity"
+                  variant="standard"
+                  fullWidth
+                  placeholder="Enter your City"
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <label>Province *</label>
+                <TextField
+                  required
+                  id="clinicProvince"
+                  variant="standard"
+                  fullWidth
+                  placeholder="Enter your Province"
+                />
+              </Grid>
+            </Grid>
+            <br></br>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+              sx={{
+                mr: 9,
+                marginLeft: 45,
+                border: 2,
+                boxShadow: 3,
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              // onClick={updateDoc_Clinic}}
+              sx={{ border: 2, boxShadow: 3 }}
+            >
+              Save Profile
+            </Button>
+          </Paper>
+          {/* </Box> */}
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
 export default ClientProfile;
